b2a26d0aa0bb5bdd7402ce43a75ba58fba2c7bdf
Tejas <tejas@callandor.io>
2023-11-22 14:47:37 +0400

    Clean up whitespace. Change num frames to 10000 in animation.

--- visualize.py
+++ visualize.py
@@ -4,26 +4,26 @@ import numpy as np
 import math
 
 
-# creating a blank window 
-# for the animation  
-fig = plt.figure()  
-axis = plt.axes(xlim =(-2, 2), 
-                ylim =(-2, 2))  
+# creating a blank window
+# for the animation
+fig = plt.figure()
+axis = plt.axes(xlim =(-2, 2),
+                ylim =(-2, 2))
   
-line, = axis.plot([], [], lw = 1)  
+line, = axis.plot([], [], lw = 1)
    
-# what will our line dataset 
-# contain? 
-def init():  
-    line.set_data([], [])  
-    return line,  
+# what will our line dataset
+# contain?
+def init():
+    line.set_data([], [])
+    return line,
 
-# initializing empty values 
-# for x and y co-ordinates 
-xdata, ydata = [], []  
+# initializing empty values
+# for x and y co-ordinates
+xdata, ydata = [], []
 
 num = math.e
-# animation function  
+# animation function
 def animate(i):  
     # pt is the complex number determined
     # by the frame number
@@ -32,22 +32,22 @@ def animate(i):
     b = 1j ** (num * theta * 2 / math.pi)
     pt = a + b
 
-    # x, y values to be plotted  
+    # x, y values to be plotted
     x = pt.real
     y = pt.imag
-       
-    # appending values to the previously  
-    # empty x and y data holders  
+
+    # appending values to the previously
+    # empty x and y data holders
     xdata.append(x)
     ydata.append(y)
-    line.set_data(xdata, ydata)  
+    line.set_data(xdata, ydata)
 
-    return line, 
+    return line,
 
-# calling the animation function      
-anim = animation.FuncAnimation(fig, animate, init_func = init,  
-                               frames = 1000, interval = 10, blit = True)  
+# calling the animation function
+anim = animation.FuncAnimation(fig, animate, init_func = init,
+                               frames = 10000, interval = 10, blit = True)
 
 animwriter = animation.FFMpegWriter(fps=30)
 # saves the animation in our desktop
-anim.save('irrational.mp4', writer = animwriter) 
+anim.save('irrational.mp4', writer = animwriter)
